bca6e12f9547151022efcf444423d8284d8731df
deepzS2 <alanr.developer@hotmail.com>
2025-06-22 00:16:52 -0300

    feat(nvim): add guihua and vectorcode support
    Adds the  plugin as a dependency for  to enable its fuzzy finding capabilities.
    
    The  plugin configuration has been updated with the  attribute for proper identification by the plugin manager.
    
    Additionally,  is used in the nushell configuration to ensure correct ordering of settings.

--- modules/home-manager/editor/nvim.nix
+++ modules/home-manager/editor/nvim.nix
@@ -133,6 +133,23 @@ in {
             trouble-nvim
             tiny-inline-diagnostic-nvim
             smear-cursor-nvim
+            (pkgs.vimUtils.buildVimPlugin {
+              pname = "guihua.lua";
+              version = "2025-06-13";
+              src = pkgs.fetchFromGitHub {
+                owner = "ray-x";
+                repo = "guihua.lua";
+                rev = "87bea7b98429405caf2a0ce4d029b027bb017c70";
+                hash = "sha256-R/ckeCwzWixvL7q2+brvqcvfSK9Mx8pu6zOFgh2lde4=";
+              };
+              buildPhase = ''
+                (
+                  cd lua/fzy
+                  make
+                )
+              '';
+              nvimSkipModules = ["fzy.fzy-lua-native"];
+            })
           ];
 
           # Editor enhancements for better coding experience
--- modules/home-manager/shell/nushell.nix
+++ modules/home-manager/shell/nushell.nix
@@ -20,6 +20,7 @@ in {
         };
 
         extraConfig =
+          lib.mkAfter
           /*
           nu
           */
