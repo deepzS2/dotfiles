f07f5cb8775ebb3a3e88b77783e7b4949d543d37
deepzS2 <alanr.developer@hotmail.com>
2025-10-11 02:22:09 -0300

    chore: maintain 3 generetions for clean

--- modules/home-manager/nix-helper.nix
+++ modules/home-manager/nix-helper.nix
@@ -2,7 +2,7 @@
   programs.nh = {
     enable = true;
     clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 1";
+    clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "${config.home.homeDirectory}/.dotfiles";
   };
 }
