955717be9b66e7bccec08898f5387f192b4808c0
deepzS2 <alanr.developer@hotmail.com>
2025-12-05 17:09:17 -0300

    chore(dev): using devshells from input of builtin

--- modules/flake/dev-shells.nix
+++ modules/flake/dev-shells.nix
@@ -1,75 +1,76 @@
 {
-  self,
-  inputs,
-  ...
-}: {
   perSystem = {pkgs, ...}: {
-    devShells = {
-      # Default development shell for working on this configuration
-      default = pkgs.mkShell {
-        name = "nixos-config-dev";
+    devshells.default = {
+      packages = builtins.attrValues {inherit (pkgs) alejandra nil statix deadnix git jq yq tree;};
+      commands = [
+        {
+          category = "styling";
+          help = "run deadnix and statix checks";
+          name = "lint";
+          command = "statix check $PRJ_ROOT && deadnix $PRJ_ROOT";
+        }
+        {
+          category = "styling";
+          help = "run nix fmt";
+          name = "fmt";
+          command = "nix fmt $PRJ_ROOT";
+        }
+        {
+          category = "flake";
+          help = "run a flake check";
+          name = "check";
+          command = "nix flake check";
+        }
+        {
+          category = "flake";
+          help = "run a flake update for all inputs or by provided in arguments";
+          name = "update";
+          command = "nix flake update $*";
+        }
+        {
+          category = "testing";
+          help = "run an nixos test for specified user";
+          name = "test";
+          command = ''
+            #!/usr/bin/env bash
 
-        packages = builtins.attrValues {
-          inherit (pkgs) alejandra nil statix deadnix git jq yq tree;
-        };
+            git add .
+            nh os test -H $1
+          '';
+        }
+        {
+          category = "testing";
+          help = "run an nixos rebuild switch";
+          name = "switch";
+          command = ''
+            #!/usr/bin/env bash
 
-        shellHook = ''
-          NH_FLAKE="$PWD"
-          alias lint="statix check $PWD && deadnix $PWD"
-          alias format="nix fmt $PWD"
+            git add .
+            nh os switch -H $1
+          '';
+        }
+        {
+          category = "testing";
+          help = "run an nixos build virtual machine";
+          name = "vm";
+          command = ''
+            #!/usr/bin/env bash
 
-          has_staged_changes() {
-            UNSTAGED_FILES=$(git status --porcelain | grep -E '^[ MADRCU][MADRCU]|^.[D]')
+            git add .
+            nh os build-vm -H $1
+          '';
+        }
+        {
+          category = "misc";
+          help = "prefetch url and get the sha256 hash";
+          name = "fetch-url-hash";
+          command = ''
+            #!/usr/bin/env bash
 
-            if [ -z "$UNSTAGED_FILES" ]; then
-              return 0
-            else
-              echo "🚨 WARNING: You have unstaged changes in your working directory."
-              echo "   Please run 'git add <file>...' or 'git add .' to stage them."
-              return 1
-            fi
-          }
-
-          test() {
-            if has_staged_changes; then
-              nh os test -H $1
-            fi
-          }
-
-          build-vm() {
-            if has_staged_changes; then
-              nh os build-vm -H $1
-            fi
-          }
-
-          switch() {
-            if has_staged_changes; then
-              nh os switch -H $1
-            fi
-          }
-
-          fetch-url-hash() {
-             nix hash convert --hash-algo sha256 $(nix-prefetch-url $1)
-          }
-
-          echo "╔════════════════════════════════════════════════════════╗"
-          echo "║   NixOS Configuration Development Environment          ║"
-          echo "╚════════════════════════════════════════════════════════╝"
-          echo ""
-          echo "Available commands:"
-          echo "  format                 - Format all Nix files"
-          echo "  nix flake check        - Validate flake configuration"
-          echo "  nix flake update       - Update all inputs"
-          echo "  lint                   - Lint Nix files for issues and unused code"
-          echo "  fetch-url-hash <url>   - Fetch URL and return hash to be used in Nix"
-          echo ""
-          echo "Build and test:"
-          echo "  build-vm <hostname>    - Build a VM Image"
-          echo "  test <hostname>        - Test current flake configuration"
-          echo "  switch <hostname>      - Rebuild and switch to the new generation"
-          echo ""
-        '';
-      };
+            nix hash convert --hash-algo sha256 $(nix-prefetch-url $1)
+          '';
+        }
+      ];
     };
   };
 }
