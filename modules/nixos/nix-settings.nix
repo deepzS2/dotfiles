57776d65c38bce81facb59ea9ce8a2064f186725
deepzS2 <alanr.developer@hotmail.com>
2025-12-12 15:25:17 -0300

    feat(nix): add binary caches

--- modules/nixos/nix-settings.nix
+++ modules/nixos/nix-settings.nix
@@ -12,6 +12,18 @@
 
       # Enable Flakes
       settings.experimental-features = ["nix-command" "flakes"];
+
+      # Binary caches for faster builds
+      settings = {
+        substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+      };
     };
   };
 }
