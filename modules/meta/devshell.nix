766101993d5496f019a1aba32d04a0b80e151452
deepzS2 <alanr.developer@hotmail.com>
2026-02-05 11:37:50 -0300

    chore(devshell): new commands and options

--- modules/meta/devshell.nix
+++ modules/meta/devshell.nix
@@ -1,7 +1,7 @@
 {
   perSystem = {pkgs, ...}: {
     devshells.default = {
-      packages = builtins.attrValues {inherit (pkgs) alejandra nil statix deadnix git jq yq tree;};
+      packages = builtins.attrValues {inherit (pkgs) alejandra nil statix deadnix git jq yq tree nix-inspect;};
       commands = [
         {
           category = "styling";
@@ -16,48 +16,98 @@
           command = "nix fmt $PRJ_ROOT";
         }
         {
-          category = "flake";
-          help = "run a flake check";
-          name = "check";
-          command = "nix flake check";
+          category = "repl";
+          help = "TUI repl inspector";
+          name = "inspector";
+          command = "nix-inspect --expr \"builtins.getFlake \\\"$PRJ_ROOT\\\"\"";
         }
         {
-          category = "flake";
-          help = "run a flake update for all inputs or by provided in arguments";
-          name = "update";
-          command = "nix flake update $*";
+          category = "repl";
+          help = "Execute nix repl on the flake";
+          name = "repl";
+          command = ''
+            #!/usr/bin/env bash
+
+            TARGET="nixos"
+            HOST=""
+
+            while [[ $# -gt 0 ]]; do
+              case $1 in
+                --target)
+                  TARGET="$2"
+                  shift 2
+                  ;;
+                --target=*)
+                  TARGET="''${1#*=}"
+                  shift
+                  ;;
+                -*)
+                  echo "Unknown option: $1"
+                  exit 1
+                  ;;
+                *)
+                  HOST="$1"
+                  shift
+                  ;;
+              esac
+            done
+
+            if [ "$TARGET" = "nixos" ]; then
+              nh os repl -H "$HOST"
+            else
+              nh home repl -c "$HOST"
+            fi
+          '';
         }
         {
-          category = "testing";
-          help = "run an nixos test for specified user";
+          category = "building";
+          help = "run an nixos build and activate";
           name = "test";
           command = ''
             #!/usr/bin/env bash
 
             git add .
-            nh os test -H $1
+            nh os test -H $1 -d always
           '';
         }
         {
-          category = "testing";
-          help = "run an nixos rebuild switch";
+          category = "building";
+          help = "run an nixos/home rebuild switch";
           name = "switch";
           command = ''
             #!/usr/bin/env bash
 
-            git add .
-            nh os switch -H $1
-          '';
-        }
-        {
-          category = "testing";
-          help = "run an nixos build virtual machine";
-          name = "vm";
-          command = ''
-            #!/usr/bin/env bash
+            TARGET="nixos"
+            HOST=""
+
+            while [[ $# -gt 0 ]]; do
+              case $1 in
+                --target)
+                  TARGET="$2"
+                  shift 2
+                  ;;
+                --target=*)
+                  TARGET="''${1#*=}"
+                  shift
+                  ;;
+                -*)
+                  echo "Unknown option: $1"
+                  exit 1
+                  ;;
+                *)
+                  HOST="$1"
+                  shift
+                  ;;
+              esac
+            done
 
             git add .
-            nh os build-vm -H $1
+
+            if [ "$TARGET" = "nixos" ]; then
+              nh os switch -H "$HOST" -d always
+            else
+              nh home switch -c "$HOST" -d always
+            fi
           '';
         }
         {
