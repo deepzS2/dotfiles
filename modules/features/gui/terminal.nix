6b5dfe36923bd6d14c71811f8a43373b6d2d0a31
deepzS2 <alanr.developer@hotmail.com>
2026-02-20 11:11:52 -0300

    feat(ghostty): native smear cursor and non-nix configuration

--- modules/features/gui/terminal.nix
+++ modules/features/gui/terminal.nix
@@ -1,21 +1,12 @@
-{
+{self, ...}: let
+  inherit (self) directories;
+in {
   flake.modules.homeManager.terminal = {pkgs, ...}: {
-    programs.ghostty = {
-      enable = true;
-      settings = {
-        theme = "Matugen";
-        background-opacity = 0.8;
-        font-family = "JetBrainsMono Nerd Font Mono";
-        font-size = 14;
-        window-padding-x = 12;
-        window-decoration = "none";
-        command = "${pkgs.nushell}/bin/nu --login --interactive";
-        confirm-close-surface = false;
-        keybind = [
-          "all:ctrl+shift+p=unbind"
-          "all:ctrl+shift+n=unbind"
-        ];
-      };
+    home.file.".config/ghostty" = {
+      source = "${directories.config}/ghostty";
+      recursive = true;
     };
+
+    programs.ghostty.enable = true;
   };
 }
