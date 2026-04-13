479da2cc530c5c937d913bbecc16ec9fae828494
deepzS2 <alanr.developer@hotmail.com>
2026-04-12 23:02:13 -0300

    feat(zen): adjustments and predefined mods

--- modules/features/gui/browser.nix
+++ modules/features/gui/browser.nix
@@ -6,7 +6,9 @@
 
     programs.zen-browser = {
       enable = true;
+      setAsDefaultBrowser = true;
       nativeMessagingHosts = [pkgs.firefoxpwa];
+
       policies = let
         mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
@@ -49,10 +51,23 @@
           "zen.view.window.scheme" = 0;
           "zen.welcome-screen.seen" = true;
         };
+        mods = [
+          "72f8f48d-86b9-4487-acea-eb4977b18f21" # Better CtrlTab
+          "a6335949-4465-4b71-926c-4a52d34bc9c0" # Better Find Bar
+          "f7c71d9a-bce2-420f-ae44-a64bd92975ab" # Better Unloaded Tabs
+          "c6813222-6571-4ba6-8faf-58f3343324f6" # Disable Rounded Corners
+          "906c6915-5677-48ff-9bfc-096a02a72379" # Floating Status Bar
+          "906c6915-5677-48ff-9bfc-096a02a72379" # Floating Status Bar
+          "79dde383-4fe7-404a-a8e6-9be440022542" # Tidy Popup
+          "4c2bec61-7f6c-4e5c-bdc6-c9ad1aba1827" # Vertical Split Tab Groups
+        ];
         search = {
           force = true;
           default = "ddg";
         };
+        spacesForce = false;
+        containersForce = false;
+        pinsForce = false;
       };
     };
   };
