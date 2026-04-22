e0c42a566a877bb9a2d86677bc93cdd31c443018
deepzS2 <alanr.developer@hotmail.com>
2026-04-22 17:35:08 -0300

    feat(terminal): ghostty systemd

--- modules/features/gui/terminal.nix
+++ modules/features/gui/terminal.nix
@@ -1,12 +1,15 @@
 {self, ...}: let
   inherit (self) directories;
 in {
-  flake.modules.homeManager.terminal = {pkgs, ...}: {
+  flake.modules.homeManager.terminal = {
     home.file.".config/ghostty" = {
       source = "${directories.config}/ghostty";
       recursive = true;
     };
 
-    programs.ghostty.enable = true;
+    programs.ghostty = {
+      enable = true;
+      systemd.enable = true;
+    };
   };
 }
